df9450b2fc57c7212a8965a5798744c02c2f82c3
DanielColorado <daniel.colorado.tostado@hotmail.com>
2016-03-28 16:42:08 -0600

    Pruebas del script para validar el formulario

--- CursoJS/getElements.js
+++ CursoJS/getElements.js
@@ -11,7 +11,7 @@ var hashTag = function (id) {
 }
 
 var dot = function (className) {
-	return document.getElementsByClassName(className);
+	return Array.prototype.slice.call(document.getElementsByClassName(className));
 }
 
 var byName = function (name) {
@@ -29,38 +29,52 @@ document.onkeyup = function () {
 }
 
 var nombre = hashTag("nombre");
-var ValidateInput = new Validate(nombre, nombre.nextElementSibling,[["required"],["minLength"]]);
-//nombre.onblur = function () {
-//
-//	ValidateInput.evalLength();
-//	ValidateInput.maxLength(10);
-//	ValidateInput.minLength(3);
-//	var msgs = nombre.nextElementSibling;
-//	ValidateInput.printMSG(msgs);
-//	ValidateInput.Clear();
-//};
+var ValidateInput = new Validate(nombre, nombre.nextElementSibling,[["required"],["minLength"]],["blur"]);
+
 
 var apellido = hashTag("apellido_paterno");
-var ValidateInputAp = new Validate(apellido, apellido.nextElementSibling,[["required"],["minLength",5]]);
-//apellido.onblur = function () {
-//
-//	ValidateInputAp.evalLength();
-//	ValidateInputAp.maxLength(10);
-//	var msgs = apellido.nextElementSibling;
-//	ValidateInputAp.printMSG(msgs);
-//	ValidateInputAp.Clear();
-//};
+var ValidateInputAp = new Validate(apellido, apellido.nextElementSibling,[["required"],["minLength",5]],["focus","keyup"]);
+
 
 var apellidoM = hashTag("apellido_materno");
-var ValidateInputAm = new Validate(apellidoM, apellidoM.nextElementSibling,[["required"],["minMaxLength",3,10]]);
-//apellidoM.onblur = function () {
-//
-//	ValidateInputAm.evalLength();
-//	ValidateInputAm.maxLength(10);
-//	var msgs = apellidoM.nextElementSibling;
-//	ValidateInputAm.printMSG(msgs);
-//	ValidateInputAm.Clear();
-//};
+var ValidateInputAm = new Validate(apellidoM, apellidoM.nextElementSibling,[["required"],["minMaxLength",3,10]],["keyup"]);
+
 
 var genere = byName("rb_genere");
-var ValidateInputG = new Validate(genere, genere[genere.length-1].nextElementSibling,[["checked"]]);
+var ValidateInputG = new Validate(genere, genere[genere.length-1].nextElementSibling.nextElementSibling,[["checked"]],["clickRB"]);
+
+var hobbies = byName("cbhobies");
+var ValidateInputH = new Validate(hobbies, hobbies[hobbies.length-1].nextElementSibling.nextElementSibling,[["checked"]],["clickRB"]);
+
+var botonEnvia = hashTag("envia");
+var formulario = hashTag("fm_nuevo");
+botonEnvia.onclick = function (event) {
+	event.preventDefault();
+	console.log(this);
+	ValidateInput.validateAll(false);
+	ValidateInputAp.validateAll(false);
+	ValidateInputAm.validateAll(false);
+	ValidateInputG.validateAll(false);
+	ValidateInputH.validateAll(false);
+
+	var arrPass = [ValidateInput.hasAnError(),ValidateInputAp.hasAnError(),ValidateInputAm.hasAnError(),ValidateInputG.hasAnError(),ValidateInputH.hasAnError()];
+
+	for(var item in arrPass){
+		if(arrPass[item] === true){
+			clean();
+			return false;
+		}
+	}
+	clean();
+	formulario.submit();
+	return true;
+}
+
+/** Funcion para limpiar los arreglos de errores **/
+function clean () {
+	ValidateInput.Clear();
+	ValidateInputAp.Clear();
+	ValidateInputAm.Clear();
+	ValidateInputG.Clear();
+	ValidateInputH.Clear();
+}
